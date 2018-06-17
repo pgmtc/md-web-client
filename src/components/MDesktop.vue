608be7d33295a2a74b7c19fef09cce91515eba91
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2018-06-17 21:04:03 +0100

    testing ci / cd

--- src/components/MDesktop.vue
+++ src/components/MDesktop.vue
@@ -3,7 +3,7 @@
     <div class="control-bar">
       <div class="title">
         <span class="user-info" v-if="user">{{user.displayName}}</span>
-        mDesktop
+        mDesktop - autoDeployed
       </div>
       <div class="controls">
         <button @click="logout()">Logout</button>
