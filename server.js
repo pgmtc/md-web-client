c5e3cd3c000155e06275fe36532c36bc8286fad1
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2018-06-21 09:15:48 +0100

    fixing startup script

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const mdWebClient = require('./index')
+const MdWebClient = require('./index')
 const PORT = process.env.PORT || 8080
 const ROOT_DIR = process.env.ROOT_DIR || './dist'
+const mdWebClient = new MdWebClient()
 mdWebClient.listen(PORT, ROOT_DIR)
